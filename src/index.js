8d08aae04a7689268fdb9ea64e707c7d8142abd4
NikolasNovo <130848761+NikolasNovo@users.noreply.github.com>
2026-01-31 22:20:26 +0200

    Refactor fetch method to handle health check

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 export default {
-  async fetch(request, env, ctx) {
-    return new Response("pockex-rates ok");
+  async fetch(request) {
+    const url = new URL(request.url);
+    if (url.pathname === "/health") return new Response("ok");
+    return new Response("not found", { status: 404 });
   }
 };
